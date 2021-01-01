3a05beb36c8eed42a58192b64f99a1a1c2465de5
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-01 09:08:07 +0300

    clean up

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/dao/feign/UserServiceFeign.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/dao/feign/UserServiceFeign.java
@@ -1,17 +1,16 @@
 package com.rsakin.getaxi.proxy.dao.feign;
 
+import com.rsakin.getaxi.proxy.dao.fallback.UserServiceFeignFallback;
 import com.rsakin.getaxi.proxy.dao.model.User;
 import com.rsakin.getaxi.proxy.dao.model.UserDTO;
-import com.rsakin.getaxi.proxy.exception.ServiceUnavailableException;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "user-service-feign", url = "localhost:9001/api/user", fallback = UserServiceFeign.UserFeignClientFallback.class)
+@FeignClient(value = "user-service-feign", url = "localhost:9001/api/user", fallback = UserServiceFeignFallback.class)
 public interface UserServiceFeign {
 
     @GetMapping(value = "/username/{username}")
@@ -20,17 +19,4 @@ public interface UserServiceFeign {
     @PostMapping(value = "/create")
     ResponseEntity<UserDTO> save(@RequestBody User user);
 
-    @Component
-    class UserFeignClientFallback implements UserServiceFeign {
-
-        @Override
-        public ResponseEntity<User> findByUsername(String username) {
-            throw new ServiceUnavailableException("User Service is unavailable now.");
-        }
-
-        @Override
-        public ResponseEntity<UserDTO> save(User user) {
-            throw new ServiceUnavailableException("User Service is unavailable now.");
-        }
-    }
 }
