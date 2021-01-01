6b5bb848cae97003eb31f2e07fc832fabc81d3a2
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-01 09:08:42 +0300

    user caching enhancement

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
@@ -4,7 +4,6 @@ import com.rsakin.getaxi.proxy.dao.feign.UserServiceFeign;
 import com.rsakin.getaxi.proxy.dao.model.User;
 import com.rsakin.getaxi.proxy.dao.model.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,22 +12,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
-    @Qualifier("user-service-feign")
     @Autowired
     private UserServiceFeign userServiceFeign;
 
     @Autowired
     private PasswordEncoder bcryptEncoder;
 
+    private static Map<String, User> userCache = new HashMap<>();
+
     @Override
     public UserDetails loadUserByUsername(String username) {
-        ResponseEntity<User> byUsername = userServiceFeign.findByUsername(username);
-        User user = byUsername.getBody();
+        User user;
+        if (userCache.get(username) != null) {
+            user = userCache.get(username);
+        } else {
+            ResponseEntity<User> byUsername = userServiceFeign.findByUsername(username);
+            user = byUsername.getBody();
+        }
         if (user == null) {
             throw new AuthenticationServiceException("Not authenticated  user [ " + username + " ]");
         }
@@ -39,6 +46,11 @@ public class JwtUserDetailsService implements UserDetailsService {
     public UserDTO save(User user) {
         user.setPassword(bcryptEncoder.encode(user.getPassword()));
         ResponseEntity<UserDTO> userDTOResponseEntity = userServiceFeign.save(user);
-        return userDTOResponseEntity.getBody();
+        UserDTO body = userDTOResponseEntity.getBody();
+        if (body != null) {
+            userCache.put(user.getUsername(), user);
+            return body;
+        }
+        return null;
     }
 }
