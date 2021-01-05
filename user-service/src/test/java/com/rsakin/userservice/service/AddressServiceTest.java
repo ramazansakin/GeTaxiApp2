12ce0f15426cb1f318be76be8528c31facd66a5e
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-05 11:33:39 +0300

    delete address unit test added

--- user-service/src/test/java/com/rsakin/userservice/service/AddressServiceTest.java
+++ user-service/src/test/java/com/rsakin/userservice/service/AddressServiceTest.java
@@ -1,6 +1,7 @@
 package com.rsakin.userservice.service;
 
 import com.rsakin.userservice.entity.Address;
+import com.rsakin.userservice.exception.AddressNotFoundException;
 import com.rsakin.userservice.repository.AddressRepository;
 import com.rsakin.userservice.service.impl.AddressServiceImpl;
 import org.junit.Before;
@@ -12,11 +13,11 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 public class AddressServiceTest {
 
@@ -72,12 +73,64 @@ public class AddressServiceTest {
 
     }
 
+    @Test
+    public void should_updateAddress() {
+
+        // stub
+        Address oldAddress = getSampleAddressList(2).get(0);
+        Address newAddress = getSampleAddressList(2).get(1);
+        newAddress.setId(1);
+
+        // when
+        Mockito.when(addressRepository.findById(oldAddress.getId()))
+                .thenReturn(java.util.Optional.of(oldAddress));
+        Mockito.when(addressRepository.save(Objects.requireNonNull(oldAddress)))
+                .thenReturn(newAddress);
+
+        // then
+        Address address = addressService.updateOne(oldAddress);
+
+        assertNotNull(address);
+        assertEquals(newAddress.getCity(), address.getCity());
+        assertEquals(newAddress.getStreet(), address.getStreet());
+        assertEquals(newAddress.getBuildingNo(), address.getBuildingNo());
+
+    }
+
+    @Test
+    public void should_NOT_updateAddress() {
+        // stub
+        Address address = getSampleAddressList(1).get(0);
+
+        // then
+        AddressNotFoundException addressNotFoundException = assertThrows(
+                AddressNotFoundException.class,
+                () -> addressService.updateOne(address),
+                "Expected doThing() to throw, but it didn't"
+        );
+
+        assertTrue(addressNotFoundException.getMessage().contains("Address not found"));
+    }
+
+    @Test
+    public void testDeleteCustomer() {
+        // stub
+        Address address = getSampleAddressList(1).get(0);
+
+        //stub the data
+        Mockito.when(addressRepository.findById(address.getId()))
+                .thenReturn(java.util.Optional.of(address));
+        doNothing().when(addressRepository).delete(address);
+
+        addressService.deleteOne(address.getId());
+    }
+
     // sample address list creation
     private List<Address> getSampleAddressList(int number) {
         List<Address> addressList = new ArrayList<>();
         for (int i = 0; i < number; i++) {
             // sample test addresses
-            addressList.add(new Address(1, "City-" + i, "Street-" + i, i + 1, (i + 1) * 5));
+            addressList.add(new Address(i + 1, "City-" + i, "Street-" + i, i + 1, (i + 1) * 5));
         }
         return addressList;
     }
