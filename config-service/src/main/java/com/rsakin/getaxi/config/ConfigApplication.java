e98f5cc99b6664b91fe9f33a8e6760961ea12c0a
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-08 18:00:26 +0300

    config-server test end : clean up

--- config-service/src/main/java/com/rsakin/getaxi/config/ConfigApplication.java
+++ config-service/src/main/java/com/rsakin/getaxi/config/ConfigApplication.java
@@ -1,31 +1,17 @@
 package com.rsakin.getaxi.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-@RestController
 public class ConfigApplication {
 
-	@Value("${sample.profile-msg:Message could not be retrieved}")
-	private String profileMsg;
-
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(ConfigApplication.class).run(args);
-	}
-
-	@GetMapping("/hello")
-	public ResponseEntity<String> hi() {
-		return new ResponseEntity<>(profileMsg, HttpStatus.OK);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApplication.class).run(args);
+    }
 
 }
