9fd0b9b20a6ecd061e12cbd2ff13e8f453498be5
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-09 14:33:26 +0300

    zuul routing bug fixed

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/ZuulGatewayApplication.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/ZuulGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
+@EnableZuulProxy
 public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
