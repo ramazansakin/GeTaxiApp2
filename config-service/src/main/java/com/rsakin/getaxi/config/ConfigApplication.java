dd9396f49129d0b8cbbeb46519c32e9402436621
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-09 08:27:06 +0300

    clean up

--- config-service/src/main/java/com/rsakin/getaxi/config/ConfigApplication.java
+++ config-service/src/main/java/com/rsakin/getaxi/config/ConfigApplication.java
@@ -3,11 +3,9 @@ package com.rsakin.getaxi.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class ConfigApplication {
 
     public static void main(String[] args) {
