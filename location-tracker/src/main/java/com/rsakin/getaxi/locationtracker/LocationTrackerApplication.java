096d919f8613d530d33d03edf7fdd9b7e2f390e3
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-11 21:11:06 +0300

    location tracker service impl

---
+++ location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/LocationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rsakin.getaxi.locationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationTrackerApplication.class, args);
+	}
+
+}
