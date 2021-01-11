a01ba858d3e98742415cfa92156d36db424ec5d7
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-11 21:39:18 +0300

    Kafka adaptation

--- location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/LocationTrackerApplication.java
+++ location-tracker/src/main/java/com/rsakin/getaxi/locationtracker/LocationTrackerApplication.java
@@ -2,12 +2,14 @@ package com.rsakin.getaxi.locationtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class LocationTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LocationTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LocationTrackerApplication.class, args);
+    }
 
 }
