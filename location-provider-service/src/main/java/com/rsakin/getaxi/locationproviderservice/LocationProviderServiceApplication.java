e24c04ced6985599e0b7c8655dd37d908893bccc
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-13 16:45:43 +0300

    location provider service init

---
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rsakin.getaxi.locationproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationProviderServiceApplication.class, args);
+	}
+
+}
