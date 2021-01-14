d532f655d326dd327b487d84a9aa8f81d4eaf690
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-14 14:47:03 +0300

    updateLocations on elastic method impl

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/consumer/LocationConsumer.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/consumer/LocationConsumer.java
@@ -1,7 +1,11 @@
 package com.rsakin.getaxi.locationproviderservice.consumer;
 
+import com.rsakin.getaxi.locationproviderservice.model.Location;
 import com.rsakin.getaxi.locationproviderservice.model.LocationDTO;
+import com.rsakin.getaxi.locationproviderservice.service.LocationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +13,27 @@ import java.util.Map;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class LocationConsumer {
 
+    private final LocationService locationService;
+
+    private final ModelMapper modelMapper;
+
     @KafkaListener(topics = "t_locations", containerFactory = "locationKafkaListenerFactory", groupId = "group_json")
-    public void consumeJson(Map<Integer, LocationDTO> locationMap) {
+    public void consumeLocations(Map<Integer, LocationDTO> locationMap) {
         log.info("Consumed Location Map : {}", locationMap);
-        // TODO : persist updated drivers' locations
+        locationMap.forEach((userId, locationDTO) -> {
+            Location byUserId = locationService.getByUserId(userId);
+            // If still there is, update it or save new one
+            if (byUserId != null) {
+                // update
+                locationService.save(byUserId);
+            } else {
+                // add new
+                locationService.save(modelMapper.map(locationDTO, Location.class));
+            }
+        });
     }
 
 }
