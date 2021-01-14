c04b66efc4aa8168d19eaad05cc6fc17bb2a98a3
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-14 20:44:57 +0300

    modelmapper bean injected

--- location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
+++ location-provider-service/src/main/java/com/rsakin/getaxi/locationproviderservice/LocationProviderServiceApplication.java
@@ -1,7 +1,9 @@
 package com.rsakin.getaxi.locationproviderservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LocationProviderServiceApplication {
@@ -10,4 +12,8 @@ public class LocationProviderServiceApplication {
         SpringApplication.run(LocationProviderServiceApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
