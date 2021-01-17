1663b03d27ca2cdc6e49739ae800c47774071d65
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-17 18:36:40 +0300

    test what env is active on config service

--- config-service/src/main/java/com/rsakin/getaxi/config/ConfigApplication.java
+++ config-service/src/main/java/com/rsakin/getaxi/config/ConfigApplication.java
@@ -1,15 +1,27 @@
 package com.rsakin.getaxi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigApplication {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigApplication.class).run(args);
+    }
+
+    @Value("${sample.profile-msg}")
+    private String profMsg;
+
+    @GetMapping("/env-check")
+    public String getEnvironmentMessage() {
+        return profMsg;
     }
 
 }
