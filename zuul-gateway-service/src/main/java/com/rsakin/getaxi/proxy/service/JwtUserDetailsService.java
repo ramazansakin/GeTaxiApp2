7bcd1f7720c394883c3a39a146d5262c90e4a781
Ramazan Sakin <ramazansakin63@gmail.com>
2021-01-20 19:54:13 +0300

    clean up

--- zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
+++ zuul-gateway-service/src/main/java/com/rsakin/getaxi/proxy/service/JwtUserDetailsService.java
@@ -3,7 +3,8 @@ package com.rsakin.getaxi.proxy.service;
 import com.rsakin.getaxi.proxy.dao.feign.UserServiceFeign;
 import com.rsakin.getaxi.proxy.dao.model.User;
 import com.rsakin.getaxi.proxy.dao.model.UserDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,15 +18,15 @@ import java.util.Map;
 
 
 @Service
+@RequiredArgsConstructor
 public class JwtUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserServiceFeign userServiceFeign;
+    @Qualifier("user-service")
+    private final UserServiceFeign userServiceFeign;
 
-    @Autowired
-    private PasswordEncoder bcryptEncoder;
+    private final PasswordEncoder bcryptEncoder;
 
-    private static Map<String, User> userCache = new HashMap<>();
+    private static final Map<String, User> userCache = new HashMap<>();
 
     @Override
     public UserDetails loadUserByUsername(String username) {
